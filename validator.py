0a447bd6f4c280c8469e0ba7884ea65ed113a6dc
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2022-11-27 16:57:33 +0100

    validator

--- validator.py
+++ validator.py
@@ -1,3 +1,20 @@
+from collections import Counter
+
+
 class Validator:
-    def __init__(self):
-        pass
+    def validWord(self, word):
+        if type(word) != str:
+            print("niepoprawne słowo")
+            return 0
+        else:
+            if self.checkStr(word):
+                return 1
+
+    def checkStr(self, word):
+        dic = Counter(word)
+        for k, v in dic.items():
+            if v > 1:
+                return 0
+        return 1
+
+
