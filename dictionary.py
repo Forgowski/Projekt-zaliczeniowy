e8c81f254e8075e02c19e3ba3b9f6ad70f0e4835
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2022-11-27 15:30:56 +0100

    random word function

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,17 @@
+import random
+
+
 class Dictionary:
     data_base = []
 
     def __init__(self):
         with open("dictionary.txt", 'r', encoding="utf-8") as file:
             holder = file.read()
-            data_base = holder.split(" ")
-            print(data_base)
+            self.data_base = holder.split(" ")
 
-Dictionary()
+    def random(self):
+        return random.choice(self.data_base)
+
+
+Dic = Dictionary()
+print(Dic.random())
